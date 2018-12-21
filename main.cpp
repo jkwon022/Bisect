2667fcee94d8776492c09f7729505ad0fbd96dd3
James Kwon <jkwon0866@gmail.com>
2018-12-21 01:42:50 -0800

    added all files

---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+//bisect the puppy image into two
+int main(int argc, char** argv){
+	string imageName;
+	if(argc > 1){
+		imageName = argv[1];
+	}
+	Mat image;
+	image = imread(imageName.c_str(), 1);
+	Mat leftHalf = image(Range::all(), Range(1,240));
+	Mat rightHalf = image(Range::all(), Range(241, 480));
+
+	namedWindow("left half", WINDOW_AUTOSIZE);
+	namedWindow("right half", WINDOW_AUTOSIZE);
+	imshow("left half", leftHalf);
+	imshow("right half", rightHalf);
+
+	return 0;
+}
